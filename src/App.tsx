60ebcb88ec006bb9d0f804b370d66d7d0d94caa0
Artchibald <archie@archibaldbutler.com>
2022-12-08 13:27:21 +0000

    ts install

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Routes, Route } from "react-router-dom"
 import { Link } from "react-router-dom";
-
 import Home from "./assets/js/Home"
 import About from "./assets/js/About"
+import Gallery from "./assets/js/Gallery"
+import Packages from "./assets/js/Packages"
 import Contact from "./assets/js/Contact"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="gallery">Gallery</Link>
+        <Link to="packages">Packages</Link>
+        <Link to="contact">Contact</Link>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="packages" element={<Packages />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
-
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="contact">Contact</Link>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> 
     </div>
   );
 }
